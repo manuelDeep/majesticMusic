e71ff7f7b998ca3e74707b7c033c363711b1ab6a
gulliverpro98 <gulliver.pro980@gmail.com>
2024-08-15 12:23:16 +0200

    Init

---
+++ src/main/java/org/majestic/majesticMusic/MajesticMusicApplication.java
@@ -0,0 +1,13 @@
+package org.majestic.majesticMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajesticMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajesticMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/majestic/majesticMusic/MajesticMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.majestic.majesticMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajesticMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
